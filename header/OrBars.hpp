cc555b5e8357826268897030ae68570f1e1fc3e3
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2019-11-15 17:20:51 -0800

    Created the OrBars.hpp file

---
+++ header/OrBars.hpp
@@ -0,0 +1,21 @@
+#ifndef _ORBARS_HPP_
+#define _ORBARS_HPP_
+
+#include <iostream>
+#include <string>
+
+#include "Connector.hpp"
+
+using namespace std;
+
+class OrBars : public Connector
+{
+	private:
+
+	public:
+		int execute()
+		{
+		};
+};
+
+#endif
