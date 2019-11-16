c45cebf6575ba961d0f21d62750831183e9a546b
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2019-11-15 17:18:14 -0800

    Created the Ampersand header

---
+++ header/Ampersand.hpp
@@ -0,0 +1,20 @@
+#ifndef _AMPERSAND_HPP_
+#define _AMPERSAND_HPP_
+
+#include <iostream>
+#include <string>
+
+#include "Connector.hpp"
+
+class Ampersand : public Connector
+{
+	private:
+
+	public:
+		int execute()
+		{
+			
+		};
+}; 
+
+#endif
