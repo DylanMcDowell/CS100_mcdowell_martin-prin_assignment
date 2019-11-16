3d345fe9d2b8ca61b962aa7f20cf1ca418fafc35
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2019-11-15 17:13:09 -0800

    Created the SemiColon header

---
+++ header/SemiColon.hpp
@@ -0,0 +1,19 @@
+#ifndef _SEMICOLON_HPP_
+#define _SEMICOLON_HPP
+
+#include <iostream>
+#include <string>
+
+#include "Connector.hpp"
+
+class SemiColon: public Connector
+{
+	private:
+
+	public:
+		int execute()
+		{
+		};
+};
+
+#endif
