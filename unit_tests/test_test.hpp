2b5b982464721c4380e18120193ce92e3e1b9198
Dylan McDowell <dmcdo006@ucr.edu>
2019-11-27 21:24:27 -0800

    Added -d flag tests and control tests

--- unit_tests/test_test.hpp
+++ unit_tests/test_test.hpp
@@ -18,6 +18,14 @@ TEST(TestTest, eFlagTest){
 	EXPECT_EQ(test.execute(), 0);
 }
 
+TEST(TestTest, eFlagDir){
+	vector<string> v;
+	v.push_back("-e");
+	v.push_back("unit_tests");
+	ShellTest test(v);
+	EXPECT_EQ(test.execute(), 0);
+}
+
 TEST(TestTest, fFlagTest){
         vector<string> v;
         v.push_back("-f");
@@ -30,6 +38,22 @@ TEST(TestTest, fFlagFail){
         vector<string> v;
         v.push_back("-f");
         v.push_back("unit_tests");
+        ShellTest test(v);
+        EXPECT_EQ(test.execute(), 1);
+}
+
+TEST(TestTest, dFlagTest){
+        vector<string> v;
+        v.push_back("-d");
+        v.push_back("unit_tests");
+        ShellTest test(v);
+        EXPECT_EQ(test.execute(), 0);
+}
+
+TEST(TestTest, dFlagFail){
+        vector<string> v;
+        v.push_back("-d");
+        v.push_back("unit_tests/test_test.hpp");
         ShellTest test(v);
         EXPECT_EQ(test.execute(), 1);
 }
